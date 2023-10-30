51d9d8cb7c693d5d0f8ab5861214840a2945dc77
tharunraj44 <51993216+tharunraj44@users.noreply.github.com>
2023-10-30 13:20:13 +0530

    Harness changes

---
+++ src/main/java/com/example/Harness/HarnessApplication.java
@@ -0,0 +1,13 @@
+package com.example.Harness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Harness/HarnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Harness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
