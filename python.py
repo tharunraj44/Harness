bcc7ecd1e2d2c93598c964f019d6e8fae13797f0
tharunpalakurthy <tharunpalakurthy@gmail.com>
2023-12-13 17:41:36 +0530

    changd python code

--- python.py
+++ python.py
@@ -1,24 +1,14 @@
 import sys
 
-# Check if the correct number of arguments is provided
-if len(sys.argv) != 3:
-    print("Usage: python filename.py -f <filename>")
-    sys.exit(1)
-
-# Extract the filename provided after the '-f' flag
-if sys.argv[1] == '-f':
-    filename = sys.argv[2]
-    print(f"Filename provided: {filename}")
-
-    # Now you can use the 'filename' variable in your script as needed
-    # For instance, if you want to read the contents of the CSV file, you might do something like this:
+if len(sys.argv) == 3 and sys.argv[1] == '-f' and sys.argv[2] == 'abc.csv':
+    # Your code for processing the provided filename goes here
+    print("Processing abc.csv file...")
+    # Example: Read the contents of abc.csv
     try:
-        with open(filename, 'r') as file:
-            # Process the CSV file here
-            pass  # Placeholder for actual processing
+        with open('abc.csv', 'r') as file:
+            data = file.read()
+            print(data)  # Just an example, you can perform any processing here
     except FileNotFoundError:
         print("File not found.")
-        sys.exit(1)
 else:
-    print("Usage: python filename.py -f <filename>")
-    sys.exit(1)
+    print("Usage: python filename.py -f abc.csv")
